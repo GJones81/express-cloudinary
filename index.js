f95e353d1e6f424cfad24ddbaf813ba739df9f22
gjones81 <jones.guy81@gmail.com>
2020-04-14 16:51:43 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+require('dotenv').config()
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+let multer = require('multer')
+let upload = multer({dest: './uploads' })
+let cloudinary = require('cloudinary')
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+cloudinary.config(process.env.CLOUDINARY_URL)
+let imgURL = cloudinary.url('mr12jyyrvb9xmdsmjban', {width: 150, height: 150})
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {image: imgURL})
 });
+
+app.post('/', upload.single('myFile'), (req, res) =>{
+	cloudinary.uploader.upload(req.file.path, (result) =>{
+		res.redirect('/')
+	})
+})
 
 app.listen(3000);
